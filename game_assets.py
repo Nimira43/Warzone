3c7f95a38efdd39cae7aed39fde6a65f14a52dd8
Nimira43 <n.rabone72@aol.com>
2025-01-15 23:36:36 +0000

    Checking tank related code

--- game_assets.py
+++ game_assets.py
@@ -10,7 +10,7 @@ class GameAssets:
     self.number_image_black_white = self.load_ind_img('numbers_black_white')        
     self.number_image_black_orange = self.load_ind_img('numbers_black_orange')        
     
-    # self.tank_images = self._load_all_tank_images()        
+    self.tank_images = self._load_all_tank_images()        
     # self.bullet_images = self._get_specified_images(self.spritesheet, gc.BULLETS, gc.BLACK)        
     # self.shield_images = self._get_specified_images(self.spritesheet, gc.SHIELD, gc.BLACK)        
     # self.spawn_star_images = self._get_specified_images(self.spritesheet, gc.SPAWN_STAR, gc.BLACK)        
@@ -35,48 +35,48 @@ class GameAssets:
     for image in ['hiScore', 'arrow', 'player1', 'player2', 'pts', 'stage', 'total']:
       self.score_sheet_images[image] = self.load_ind_img(image)
 
-  # def _load_all_tank_images(self):
-  #   tank_image_dict = {}
-  #   for tank in range(8):
-  #     tank_image_dict[f'Tank_{tank}'] = {}
-  #     for group in ['Gold', 'Silver', 'Green', 'Special']:
-  #       tank_image_dict[f'Tank_{tank}'][group] = {}
-  #       for direction in ['Up', 'Down', 'Left', 'Right']:
-  #         tank_image_dict[f'Tank_{tank}'][group][direction] = []
+  def _load_all_tank_images(self):
+    tank_image_dict = {}
+    for tank in range(8):
+      tank_image_dict[f'Tank_{tank}'] = {}
+      for group in ['Gold', 'Silver', 'Green', 'Special']:
+        tank_image_dict[f'Tank_{tank}'][group] = {}
+        for direction in ['Up', 'Down', 'Left', 'Right']:
+          tank_image_dict[f'Tank_{tank}'][group][direction] = []
     
-  #   for row in range(16):
-  #     for col in range(16):
-  #       surface = pygame.Surface((gc.spriteSize, gc.spriteSize))
-  #       surface.fill(gc.BLACK)
-  #       surface.blit(self.spritesheet, (0, 0), (col * gc.spriteSize, row * gc.spriteSize, gc.spriteSize, gc.spriteSize))
+    for row in range(16):
+      for col in range(16):
+        surface = pygame.Surface((gc.spriteSize, gc.spriteSize))
+        surface.fill(gc.BLACK)
+        surface.blit(self.spritesheet, (0, 0), (col * gc.spriteSize, row * gc.spriteSize, gc.spriteSize, gc.spriteSize))
                                                 
-  #       surface.set_colorkey(gc.BLACK)
-  #       surface = self.scale_image(surface, gc.spriteScale)
-  #       tank_level = self._sort_tanks_into_levels(row)
-  #       tank_group = self._sort_tanks_into_groups(row, col)
-  #       tank_direction = self._sort_tanks_by_direction(col)
-  #       tank_image_dict[tank_level][tank_group][tank_direction].append(surface)
-  #   return tank_image_dict
+        surface.set_colorkey(gc.BLACK)
+        surface = self.scale_image(surface, gc.spriteScale)
+        tank_level = self._sort_tanks_into_levels(row)
+        tank_group = self._sort_tanks_into_groups(row, col)
+        tank_direction = self._sort_tanks_by_direction(col)
+        tank_image_dict[tank_level][tank_group][tank_direction].append(surface)
+    return tank_image_dict
       
-  # def scale_image(self, image, scale):
-  #   width, height = image.get_size()
-  #   image = pygame.transform.scale(image, (scale * width, scale * height))
-  #   return image
+  def scale_image(self, image, scale):
+    width, height = image.get_size()
+    image = pygame.transform.scale(image, (scale * width, scale * height))
+    return image
   
-  # def _sort_tanks_into_levels(self, row):
-  #   tank_levels = {0: 'Tank_0', 1: 'Tank_1', 2: 'Tank_2', 3: 'Tank_3', 4: 'Tank_4', 5: 'Tank_5', 6: 'Tank_6', 7: 'Tank_7'}
+  def _sort_tanks_into_levels(self, row):
+    tank_levels = {0: 'Tank_0', 1: 'Tank_1', 2: 'Tank_2', 3: 'Tank_3', 4: 'Tank_4', 5: 'Tank_5', 6: 'Tank_6', 7: 'Tank_7'}
                     
-  #   return tank_levels[row % 8]
+    return tank_levels[row % 8]
   
-  # def _sort_tanks_into_groups(self, row, col):
-  #   if 0 <= row <= 7 and 0 <= col <= 7:
-  #     return 'Gold'
-  #   elif 8 <= row <= 16 and 0 <= col <= 7:
-  #     return 'Green'
-  #   elif 0 <= row <= 7 and 8 <= col <= 16:
-  #     return 'Silver'
-  #   else:
-  #     return 'Special'
+  def _sort_tanks_into_groups(self, row, col):
+    if 0 <= row <= 7 and 0 <= col <= 7:
+      return 'Gold'
+    elif 8 <= row <= 16 and 0 <= col <= 7:
+      return 'Green'
+    elif 0 <= row <= 7 and 8 <= col <= 16:
+      return 'Silver'
+    else:
+      return 'Special'
       
   # def _sort_tanks_by_direction(self, col):
   #   if col % 7 <= 1: return 'Up'
