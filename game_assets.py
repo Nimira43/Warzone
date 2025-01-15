1c4587d73aa4f50f9f0aad46488e9ae40a460207
Nimira43 <n.rabone72@aol.com>
2025-01-15 23:56:06 +0000

    More code brought back into game_config and game_assets.py. Some correctsions made

--- game_assets.py
+++ game_assets.py
@@ -11,25 +11,25 @@ class GameAssets:
     self.number_image_black_orange = self.load_ind_img('numbers_black_orange')        
     
     self.tank_images = self._load_all_tank_images()        
-    # self.bullet_images = self._get_specified_images(self.spritesheet, gc.BULLETS, gc.BLACK)        
-    # self.shield_images = self._get_specified_images(self.spritesheet, gc.SHIELD, gc.BLACK)        
-    # self.spawn_star_images = self._get_specified_images(self.spritesheet, gc.SPAWN_STAR, gc.BLACK)        
-    # self.power_up_images = self._get_specified_images(self.spritesheet, gc.POWER_UPS, gc.BLACK)        
+    self.bullet_images = self._get_specified_images(self.spritesheet, gc.BULLETS, gc.BLACK)        
+    self.shield_images = self._get_specified_images(self.spritesheet, gc.SHIELD, gc.BLACK)        
+    self.spawn_star_images = self._get_specified_images(self.spritesheet, gc.SPAWN_STAR, gc.BLACK)        
+    self.power_up_images = self._get_specified_images(self.spritesheet, gc.POWER_UPS, gc.BLACK)        
     
-    # self.flag = self._get_specified_images(self.spritesheet, gc.FLAG, gc.BLACK)        
-    # self.explosions = self._get_specified_images(self.spritesheet, gc.EXPLOSIONS, gc.BLACK)        
-    # self.score = self._get_specified_images(self.spritesheet, gc.SCORE, gc.BLACK)        
-    # self.hud_images = self._get_specified_images(self.spritesheet, gc.HUD_INFO, gc.BLACK, transparent=False)        
-    # self.context = self._get_specified_images(self.spritesheet, gc.CONTEXT, gc.BLACK)     
+    self.flag = self._get_specified_images(self.spritesheet, gc.FLAG, gc.BLACK)        
+    self.explosions = self._get_specified_images(self.spritesheet, gc.EXPLOSIONS, gc.BLACK)        
+    self.score = self._get_specified_images(self.spritesheet, gc.SCORE, gc.BLACK)        
+    self.hud_images = self._get_specified_images(self.spritesheet, gc.HUD_INFO, gc.BLACK, transparent=False)        
+    self.context = self._get_specified_images(self.spritesheet, gc.CONTEXT, gc.BLACK)     
 
-    # self.brick_tiles = self._get_specified_images(self.spritesheet, gc.MAP_TILES[432], gc.BLACK)        
-    # self.steel_tiles = self._get_specified_images(self.spritesheet, gc.MAP_TILES[482], gc.BLACK)        
-    # self.forest_tiles = self._get_specified_images(self.spritesheet, gc.MAP_TILES[483], gc.BLACK)        
-    # self.ice_tiles = self._get_specified_images(self.spritesheet, gc.MAP_TILES[484], gc.BLACK)        
-    # self.water_tiles = self._get_specified_images(self.spritesheet, gc.MAP_TILES[533], gc.BLACK) 
+    self.brick_tiles = self._get_specified_images(self.spritesheet, gc.MAP_TILES[432], gc.BLACK)        
+    self.steel_tiles = self._get_specified_images(self.spritesheet, gc.MAP_TILES[482], gc.BLACK)        
+    self.forest_tiles = self._get_specified_images(self.spritesheet, gc.MAP_TILES[483], gc.BLACK)        
+    self.ice_tiles = self._get_specified_images(self.spritesheet, gc.MAP_TILES[484], gc.BLACK)        
+    self.water_tiles = self._get_specified_images(self.spritesheet, gc.MAP_TILES[533], gc.BLACK) 
 
-    # self.number_black_white = self._get_specified_images(self.number_image_black_white, gc.NUMS, gc.BLACK)
-    # self.number_black_orange = self._get_specified_images(self.number_image_black_orange, gc.NUMS, gc.BLACK)
+    self.number_black_white = self._get_specified_images(self.number_image_black_white, gc.NUMS, gc.BLACK)
+    self.number_black_orange = self._get_specified_images(self.number_image_black_orange, gc.NUMS, gc.BLACK)
 
     self.score_sheet_images = {}
     for image in ['hiScore', 'arrow', 'player1', 'player2', 'pts', 'stage', 'total']:
@@ -78,28 +78,28 @@ class GameAssets:
     else:
       return 'Special'
       
-  # def _sort_tanks_by_direction(self, col):
-  #   if col % 7 <= 1: return 'Up'
-  #   elif col % 7 <= 3: return 'Left'
-  #   elif col % 7 <= 5: return 'Down'
-  #   else:
-  #     return 'Right'
+  def _sort_tanks_by_direction(self, col):
+    if col % 7 <= 1: return 'Up'
+    elif col % 7 <= 3: return 'Left'
+    elif col % 7 <= 5: return 'Down'
+    else:
+      return 'Right'
   
-  # def _get_specified_images(self, spritesheet, img_coord_dict, color, transparent=True):
-  #   image_dictionary = {}
-  #   for key, pos in img_coord_dict.items():
-  #     image = self.get_image(spritesheet, pos[0], pos[1], pos[2], pos[3], color, transparent)
-  #     image_dictionary.setdefault(key, image)
-  #   return image_dictionary
+  def _get_specified_images(self, spritesheet, img_coord_dict, color, transparent=True):
+    image_dictionary = {}
+    for key, pos in img_coord_dict.items():
+      image = self.get_image(spritesheet, pos[0], pos[1], pos[2], pos[3], color, transparent)
+      image_dictionary.setdefault(key, image)
+    return image_dictionary
   
-  # def get_image(self, spritesheet, xpos, ypos, width, height, color, transparent=True):
-  #   surface = pygame.Surface((width, height))
-  #   surface.fill(color)
-  #   surface.blit(spritesheet, (0, 0), (xpos, ypos, width, height))
-  #   if transparent:
-  #     surface.set_colorkey(color)
-  #   surface = self.scale_image(surface, gc.spriteScale)
-  #   return surface
+  def get_image(self, spritesheet, xpos, ypos, width, height, color, transparent=True):
+    surface = pygame.Surface((width, height))
+    surface.fill(color)
+    surface.blit(spritesheet, (0, 0), (xpos, ypos, width, height))
+    if transparent:
+      surface.set_colorkey(color)
+    surface = self.scale_image(surface, gc.spriteScale)
+    return surface
 
   def load_ind_img(self, path, scale=False, size=(0,  0)):
     image = pygame.image.load(f'assets/{path}.png').convert_alpha()
