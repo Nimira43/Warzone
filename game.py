0e123952d1922499976a022ffeb907b1eb6851d7
Nimira43 <n.rabone72@aol.com>
2025-01-16 00:55:53 +0000

    Modified game.py

--- game.py
+++ game.py
@@ -1,19 +1,19 @@
 import pygame
 import game_config as gc
-# from characters import Tank, PlayerTank
+from characters import Tank, PlayerTank
 
 class Game:
   def __init__(self, main, assets):
     self.main = main
     self.assets = assets
-    # self.groups = {'All_Tanks': pygame.sprite.Group()}
-    # self.player1 = PlayerTank(self, self.assets, self.groups, (200, 200), 'Up', 'Gold', 0)
-    # self.player2 = PlayerTank(self, self.assets, self.groups, (400, 200), 'Up', 'Green', 1)
+    self.groups = {'All_Tanks': pygame.sprite.Group()}
+    self.player1 = PlayerTank(self, self.assets, self.groups, (200, 200), 'Up', 'Gold', 0)
+    self.player2 = PlayerTank(self, self.assets, self.groups, (400, 200), 'Up', 'Green', 1)
 
   def input(self):
-    # keypressed = pygame.key.get_pressed()
-    # self.player1.input(keypressed)
-    # self.player2.input(keypressed)
+    keypressed = pygame.key.get_pressed()
+    self.player1.input(keypressed)
+    self.player2.input(keypressed)
 
     for event in pygame.event.get():
       if event.type == pygame.QUIT:
@@ -23,11 +23,9 @@ class Game:
           self.main.run = False
 
   def update(self):
-    # self.player1.update()
-    # self.player2.update()
-    print('Game running')
+    self.player1.update()
+    self.player2.update()
 
   def draw(self, window):
-    # self.player1.draw(window)
-    # self.player2.draw(window)
-    pass
+    self.player1.draw(window)
+    self.player2.draw(window)
