c417635991b4f9c2d5628b0d16cccb4be3d6910b
Nimira43 <n.rabone72@aol.com>
2025-01-16 01:24:28 +0000

    Brought forward Phase 8 code and will review code, making corrections where needed.

--- characters.py
+++ characters.py
@@ -1,9 +1,10 @@
 import pygame
+from ammunition import Bullet
 import game_config as gc
 
 class Tank(pygame.sprite.Sprite):
 
-    def __init__(self, game, assets, groups, position, direction, colour='Silver', tank_level=0):
+    def __init__(self, game, assets, groups, position, direction, enemy = True, colour='Silver', tank_level=0):
         super().__init__()
         self.game = game
         self.assets = assets
@@ -11,8 +12,8 @@ class Tank(pygame.sprite.Sprite):
 
         self.tank_group = self.groups['All_Tanks']
         self.tank_group.add(self)
-
         self.tank_images = self.assets.tank_images
+        
         self.spawn_images = self.assets.spawn_star_images
         self.spawn_pos = position
         self.xPos, self.yPos = self.spawn_pos
@@ -24,14 +25,29 @@ class Tank(pygame.sprite.Sprite):
         self.tank_level = tank_level
         self.colour = colour
         self.tank_speed = gc.TANK_SPEED
+        self.enemy = enemy
+        self.tank_health = 1
 
         self.frame_index = 0
         self.image = self.tank_images[f'Tank_{self.tank_level}'][self.colour][self.direction][self.frame_index]
         self.rect = self.image.get_rect(topleft=(self.spawn_pos))
+        self.width, self.height = self.image.get_size()
+
+        self.bullet_limit = 1
+        self.bullet_sum = 0
+
+        self.paralyzed = False
+        self.paralysis = gc.TANK_PARALYSIS
+        self.paralysis_timer = pygame.time.get_ticks()
 
         self.spawn_image = self.spawn_images[f'star_{self.frame_index}']
         self.spawn_timer = pygame.time.get_ticks()
         self.spawn_anim_timer = pygame.time.get_ticks()
+
+        self.mask_dict = self.get_various_masks()
+        self.mask = self.mask_dict[self.direction]
+        # self.mask_image = self.mask.to_surface()
+        self.mask_direction = self.direction
 
     def input(self):
         pass
@@ -44,7 +60,10 @@ class Tank(pygame.sprite.Sprite):
                 self.frame_index = 0
                 self.spawning = False
                 self.active = True
-        return
+            return
+        if self.paralyzed:
+            if pygame.time.get_ticks() - self.paralysis_timer >= self.paralysis:
+                self.paralyzed = False
 
     def draw(self, window):
         if self.spawning:
@@ -52,21 +71,32 @@ class Tank(pygame.sprite.Sprite):
 
         if self.active:
             window.blit(self.image, self.rect)
+            # window.blit(self.mask_image, self.rect)
             pygame.draw.rect(window, gc.RED, self.rect, 1)
 
     def move_tank(self, direction):
         if self.spawning:
             return
-        self.direction = direction
-
+        self.direction = direction        
+        if self.paralyzed:
+            self.image = self.tank_images[f'Tank_{self.tank_level}'][self.colour][self.direction][self.frame_index]
+            return
         if direction == 'Up':
             self.yPos -= self.tank_speed
+            if self.yPos < gc.SCREEN_BORDER_TOP:
+                self.yPos = gc.SCREEN_BORDER_TOP
         elif direction == 'Down':
             self.yPos += self.tank_speed
+            if self.yPos + self.height > gc.SCREEN_BORDER_BOTTOM:
+                self.yPos = gc.SCREEN_BORDER_BOTTOM - self.height
         elif direction == 'Left':
             self.xPos -= self.tank_speed
+            if self.xPos < gc.SCREEN_BORDER_LEFT:
+                self.xPos = gc.SCREEN_BORDER_LEFT
         elif direction == 'Right':
             self.xPos += self.tank_speed
+            if self.xPos + self.width > gc.SCREEN_BORDER_RIGHT:
+                self.xPos = gc.SCREEN_BORDER_RIGHT - self.width
         
         self.rect.topleft = (self.xPos, self.yPos)
         self.tank_movement_animation()
@@ -76,13 +106,24 @@ class Tank(pygame.sprite.Sprite):
         self.frame_index += 1
         imagelistlength = len(self.tank_images[f'Tank_{self.tank_level}'][self.colour][self.direction])
         self.frame_index = self.frame_index % imagelistlength
-        self.image = self.tank_images[f'Tank_{self.tank_level}'][self.colour][self.direction][self.frame_index]  
+        self.image = self.tank_images[f'Tank_{self.tank_level}'][self.colour][self.direction][self.frame_index]
+        if self.mask_direction != self.direction:
+            self.mask_direction = self.direction
+            self.mask = self.mask_dict[self.mask_direction]
+            # self.mask_image = self.mask.to_surface()  
 
     def spawn_animation(self): 
         self.frame_index += 1
         self.frame_index = self.frame_index % len(self.spawn_images)
         self.spawn_image = self.spawn_images[f'star_{self.frame_index}']
         self.spawn_anim_timer = pygame.time.get_ticks()
+
+    def get_various_masks(self):
+        images = {}
+        for direction in ['Up', 'Down', 'Left', 'Right']:
+            image_to_mask = self.tank_images[f'Tank_{self.tank_level}'][self.colour][direction][0]
+            images.setdefault(direction, pygame.mask.from_surface(image_to_mask))
+        return images
 
     def tank_on_tank_collisions(self):
         tank_collision = pygame.sprite.spritecollide(self, self.tank_group, False)
@@ -116,9 +157,27 @@ class Tank(pygame.sprite.Sprite):
                     self.rect.bottom = tank.rect.top
                     self.yPos = self.rect.y
 
+    def shoot(self):
+        if self.bullet_sum >= self.bullet_limit:
+            return
+        bullet = Bullet(self.groups, self, self.rect.center, self.direction, self.assets)
+        self.bullet_sum += 1
+
+    def paralyze_tank(self, paralysis_time):
+        self.paralysis = paralysis_time
+        self.paralyzed = True
+        self.paralysis_timer = pygame.time.get_ticks()
+
+    def destroy_tank(self):
+        self.tank_health -= 1
+        if self.tank_health <= 0:
+            self.kill()
+            return
+
 class PlayerTank(Tank):
     def __init__(self, game, assets, groups, position, direction, colour, tank_level):
-        super().__init__(game, assets, groups, position, direction, colour, tank_level)
+        super().__init__(game, assets, groups, position, direction, False, colour, tank_level)
+        self.lives = 3
     
     def input(self, keypressed):
         if self.colour == 'Gold':
--- game.py
+++ game.py
@@ -1,31 +1,69 @@
 import pygame
 import game_config as gc
 from characters import Tank, PlayerTank
+from game_hud import GameHud
 
 class Game:
-  def __init__(self, main, assets):
-    self.main = main
-    self.assets = assets
-    self.groups = {'All_Tanks': pygame.sprite.Group()}
-    self.player1 = PlayerTank(self, self.assets, self.groups, (200, 200), 'Up', 'Gold', 0)
-    self.player2 = PlayerTank(self, self.assets, self.groups, (400, 200), 'Up', 'Green', 1)
+    def __init__(self, main, assets, player1=True, player2=False):
+        self.main = main
+        self.assets = assets
 
-  def input(self):
-    keypressed = pygame.key.get_pressed()
-    self.player1.input(keypressed)
-    self.player2.input(keypressed)
+        self.groups = {'All_Tanks': pygame.sprite.Group(),
+                       'Bullets': pygame.sprite.Group()}
+        self.player1_active = player1
+        self.player2_active = player2
 
-    for event in pygame.event.get():
-      if event.type == pygame.QUIT:
-        self.main.run = False
-      if event.type == pygame.KEYDOWN:
-        if event.key == pygame.K_ESCAPE:
-          self.main.run = False
+        self.hud = GameHud(self, self.assets)
+        self.level_num = 1
+        
+        if self.player1_active:
+            self.player1 = PlayerTank(self, self.assets, self.groups, (200, 200), 'Up', 'Gold', 0)
+        if self.player2_active:
+            self.player2 = PlayerTank(self, self.assets, self.groups, (400, 200), 'Up', 'Green', 1)
 
-  def update(self):
-    self.player1.update()
-    self.player2.update()
+        self.enemies = 20
 
-  def draw(self, window):
-    self.player1.draw(window)
-    self.player2.draw(window)
+    def input(self):
+        keypressed = pygame.key.get_pressed()
+        if self.player1_active:
+            self.player1.input(keypressed)
+        if self.player2_active:
+            self.player2.input(keypressed)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                self.main.run = False
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    self.main.run = False
+                if event.key == pygame.K_w:
+                    if self.player1_active:
+                        self.player1.shoot()
+                if event.key == pygame.K_o:
+                    if self.player2_active:
+                        self.player2.shoot()
+
+                if event.key == pygame.K_RETURN:
+                    Tank(self, self.assets, self.groups, (400, 400), 'Down')
+                    self.enemies -= 1
+
+    def update(self):
+        self.hud.update()
+        # if self.player1_active:
+        #     self.player1.update()
+        # if self.player2_active:
+        #     self.player2.update()
+        for dictKey in self.groups.keys():
+            for item in self.groups[dictKey]:
+                item.update()
+
+    def draw(self, window):
+        self.hud.draw(window)
+        # if self.player1_active:
+        #     self.player1.draw(window)
+        # if self.player2_active:
+        #     self.player2.draw(window)
+        for dictKey in self.groups.keys():
+            for item in self.groups[dictKey]:
+                item.draw(window)
+
