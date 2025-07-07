35a647726c1b5e8cda27bd00c867dfe5480d17ad
Nimira43 <n.rabone72@aol.com>
2025-07-07 21:07:07 +0100

    Creating outline for LevelData class

--- levels.py
+++ levels.py
@@ -2,3 +2,14 @@ import pygame
 import os
 import csv
 
+class LevelData:
+  def __init__(self):
+    pass
+
+  def load_level_data(self):
+    pass
+
+  def save_level_data(self, level_data):
+    pass
+
+  
