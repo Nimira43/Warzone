6259359df164bf960c5e6ea1ea7337fa28cdf0ea
Nimira43 <n.rabone72@aol.com>
2025-07-08 00:19:38 +0100

    Outlining StartScreen class and its functions

---
+++ startscreen.py
@@ -0,0 +1,27 @@
+import pygame
+import game_config as gc
+
+class StartScreen:
+  def __init__(self, main, assets):
+    pass
+
+  def input(self):
+    pass
+
+  def update(self):
+    pass
+
+  def draw(self, window):
+    pass
+
+  def _switch_options_main_menu(self, num):
+    pass
+
+  def _selected_option_action(self):
+    pass
+
+  def _animate_screen_into_position(self):
+    pass
+
+  def _complete_screen_position(self):
+    pass
