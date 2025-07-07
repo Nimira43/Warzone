39eedde21a28845e5d184ced470e2e633fd98e55
Nimira43 <n.rabone72@aol.com>
2025-07-07 21:22:55 +0100

    save_level_data function completed for now

--- levels.py
+++ levels.py
@@ -19,5 +19,11 @@ class LevelData:
     return
 
   def save_level_data(self, level_data):
-    pass
-
+    number = len(level_data)
+    for i in range(number):
+      num = i + 1 if len(str(i + 1)) > 1 else '0' + str(i + 1)
+      with open(f'levels/BattleCityLevel{str(num)}.csv', 'w', newline='') as csvFile:
+        writer = csv.writer(csvFile, delimiter=',')
+        for row in level_data[i]:
+          writer.writerow(row)
+    return
