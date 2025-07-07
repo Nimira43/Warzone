23410878ccdffe838ec885780356591e12c5f40a
Nimira43 <n.rabone72@aol.com>
2025-07-07 21:12:42 +0100

    load_level_data function

--- levels.py
+++ levels.py
@@ -4,12 +4,20 @@ import csv
 
 class LevelData:
   def __init__(self):
-    pass
+    self.leveldata = self.load_level_data()
 
   def load_level_data(self):
-    pass
+    gameStages = []
+    for stage in os.listdir('levels'):
+      level_data = [[] for i in range(27)]
+      with open(f'levels/{stage}', newline='') as csvFile:
+        reader = csv.reader(csvFile, delimiter=',')
+        for i, row in enumerate(reader):
+          for j, tile in enumerate(row):
+            level_data[i].append(int(tile))
+      gameStages.append(level_data)
+    return
 
   def save_level_data(self, level_data):
     pass
 
-  
