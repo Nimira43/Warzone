cd657a4c8fb67ccbed7ea2712716146bfed4086b
Nimira43 <n.rabone72@aol.com>
2025-07-08 00:24:18 +0100

    Writing init function. New variable - SCREEN_SCROLL_SPEED

--- startscreen.py
+++ startscreen.py
@@ -3,7 +3,14 @@ import game_config as gc
 
 class StartScreen:
   def __init__(self, main, assets):
-    pass
+    self.main = main
+    self.assets = assets
+    self.start_y = gc.SCREENHEIGHT
+    self.end_y = 0
+    self.image = self.assets.start_screen
+    self.rect = self.image.get_rect(topleft=(0, self.start_y))
+    self.x, self.y = self.rect.topleft
+    self.speed = gc.SCREEN_SCROLL_SPEED
 
   def input(self):
     pass
