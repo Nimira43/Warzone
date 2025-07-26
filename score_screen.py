f16912db74577a497320903a6aced6216c87deae
Nimira43 <n.rabone72@aol.com>
2025-07-26 04:43:38 +0100

    Outlining methods for ScoreScreen class.

--- score_screen.py
+++ score_screen.py
@@ -2,5 +2,38 @@ import pygame
 import game_config as gc
 
 class ScoreScreen:
-  pass
+  def __init(self, game, assets):
+    pass
+
+  def update(self):
+    pass
+
+  def draw(self, window):
+    pass
+
+  def generate_scoresheet_screen(self):
+    pass
+
+  def number_image(self, score, number_colour):
+    pass
+
+  def update_player_score_images(self):
+    pass
+
+  def _create_top_score_and_stage_number_images(self):
+    pass
+
+  def update_basic_info(self, top_score, stage_number):
+    pass
+
+  def generate_tank_kill_images(self, x1, x2, pl_dict):
+    pass
+
+  def update_score(self, score, player):
+    pass
+
+  def clear_for_new_stage(self):
+    pass
+
+  
 
