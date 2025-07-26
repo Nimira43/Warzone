65642617a07a82349d05f14341d16d491f3e5aee
Nimira43 <n.rabone72@aol.com>
2025-07-26 04:02:06 +0100

    Outlining TileType methods

--- tile.py
+++ tile.py
@@ -2,7 +2,20 @@ import pygame
 import game_config as gc
 
 class TileType(pygame.sprite.Sprite):
-  pass
+  def __init__(self, pos, group, map_tile):
+    pass
+
+  def update(self):
+    pass
+
+  def _get_rect_and_size(self, position):
+    pass
+
+  def hit_by_bullet(self, bullet):
+    pass
+
+  def draw(self, window):
+    pass
 
 class BrickTile(TileType):
   pass
