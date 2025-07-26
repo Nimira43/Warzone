75c3ce6aa10b969685ef58f4c8c921d94d6374bc
Nimira43 <n.rabone72@aol.com>
2025-07-26 03:59:41 +0100

    Creating different classes

---
+++ tile.py
@@ -0,0 +1,20 @@
+import pygame
+import game_config as gc
+
+class TileType(pygame.sprite.Sprite):
+  pass
+
+class BrickTile(TileType):
+  pass
+
+class SteelTile(TileType):
+  pass
+
+class ForestTile(TileType):
+  pass
+
+class IceTile(TileType):
+  pass
+
+class WaterTile(TileType):
+  pass
