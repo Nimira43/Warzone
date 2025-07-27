c11ba21f6605917bfbb06d98db023a9bb52154f0
Nimira43 <n.rabone72@aol.com>
2025-07-27 21:07:08 +0100

    Outlined Fade class and methods.

--- fade_animate.py
+++ fade_animate.py
@@ -1,2 +1,20 @@
 import pygame
 import game_config as gc
+
+class Fade:
+  def __init__(self, game, assets, speed = 5):
+    pass
+
+  def update(self):
+    pass
+
+  def draw(self, window):
+    pass
+
+  def move_y_fade(self, ycoord, start_pos, end_pos, speed):
+    pass
+
+  def create_stage_image(self):
+    pass
+
+  
