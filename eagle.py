b01d25e3e26e16f7b1a9b044ae712ee147d61d59
Nimira43 <n.rabone72@aol.com>
2025-08-23 21:29:33 +0100

    Parameters passed into Eagle class and methods

--- eagle.py
+++ eagle.py
@@ -2,15 +2,15 @@ import pygame
 from explosions import Explosion
 import game_config as gc
 
-class Eagle():
-  def __init__():
+class Eagle(pygame.sprite.Sprite):
+  def __init__(self, game, assets, groups):
     pass
 
-  def update():
+  def update(self):
     pass
 
-  def draw():
+  def draw(self, window):
     pass
 
-  def destroy_base():
+  def destroy_base(self):
     pass
