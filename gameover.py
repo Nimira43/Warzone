bf9bb9ed730b7b39ffd526954241ede0d151a230
Nimira43 <n.rabone72@aol.com>
2025-08-23 21:48:15 +0100

    GameOver init method done.

--- gameover.py
+++ gameover.py
@@ -2,8 +2,15 @@ import pygame
 import game_config as gc
 
 class GameOver:
-  def __init__():
-    pass
+  def __init__(self, game, assets):
+    self.game = game
+    self.assets = assets
+    self.game_over_image = self.assets.context['game_over']
+    self.game_over_rect = self.game_over_image.get_rect()
+    self.g_width, self.g_height = self.game_over_image.get_size()
+    self.game_over_rect.center = (gc.SCREENWIDTH // 2 - self.g_width // 2, gc.SCREENHEIGHT + self.g_height)
+    self.timer = pygame.time.get_ticks()
+    self.active = False
 
   def activate():
     pass
