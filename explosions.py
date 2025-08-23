c8ac2457698e8a090476aefc2d5d75b4abac327a
Nimira43 <n.rabone72@aol.com>
2025-08-23 21:14:58 +0100

    Explode init - 1

--- explosions.py
+++ explosions.py
@@ -3,7 +3,15 @@ from scores import ScoreBanner
 
 class Explosion(pygame.sprite.Sprite):
   def __init__(self, assets, group, pos, explode_type=1, score=100):
-    pass
+    super().__init__()
+    self.assets = assets
+    self.group = group
+    self.explosion_group = self.group['Explosion']
+    self.explosion_group.add(self)
+    self.score = score
+    self.pos = pos
+    self.explode_type = explode_type
+    
 
   def update(self):
     pass
