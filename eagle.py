c06c2ea3dd4ea5224e51dd26fad937d4184480dd
Nimira43 <n.rabone72@aol.com>
2025-08-23 21:32:52 +0100

    Eagle init complete

--- eagle.py
+++ eagle.py
@@ -4,7 +4,15 @@ import game_config as gc
 
 class Eagle(pygame.sprite.Sprite):
   def __init__(self, game, assets, groups):
-    pass
+    super().__init__()
+    self.game = game
+    self.assets = assets
+    self.group = groups
+    self.group['Eagle'].add(self)
+    self.active = True
+    self.timer = pygame.time.get_ticks()
+    self.image = self.assets.flag['Phoenix_Alive']
+    self.rect = self.image.get_rect(topleft=(gc.FLAG_POSITION))
 
   def update(self):
     pass
