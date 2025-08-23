a40ee9ddeb5e2c921683b8e1ad7b71589700e44c
Nimira43 <n.rabone72@aol.com>
2025-08-23 22:04:23 +0100

    PowerUps class - init - 2

--- powerups.py
+++ powerups.py
@@ -6,7 +6,13 @@ from scores import ScoreBanner
 class PowerUps(pygame.sprite.Sprite):
   def __init__(self, game, assets, groups):
     super().__init__()
-    
+    self.game = game
+    self.assets = assets
+    self.powerup_images = self.assets.power_up_images
+
+    self.groups = groups
+    self.groups['Power_Ups'].add(self)
+
 
   def randomly_select_power_up():
     pass
