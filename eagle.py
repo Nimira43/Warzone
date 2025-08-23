863e98ceaa97faf9569269802662920092c99c86
Nimira43 <n.rabone72@aol.com>
2025-08-23 20:38:38 +0100

    Outline for Eagle class and methods

--- eagle.py
+++ eagle.py
@@ -1,3 +1,16 @@
 import pygame
 from explosions import Explosion
 import game_config as gc
+
+class Eagle():
+  def __init__():
+    pass
+
+  def update():
+    pass
+
+  def draw():
+    pass
+
+  def destroy_base():
+    pass
