716d5921e0c6a6b419d13460787b86cd55938d8a
Nimira43 <n.rabone72@aol.com>
2025-08-23 20:46:29 +0100

    PowerUps class with first lot of methods outlined.

--- powerups.py
+++ powerups.py
@@ -2,3 +2,19 @@ import pygame
 import random
 import game_config as gc
 from scores import ScoreBanner
+
+class PowerUps():
+  def __init__():
+    pass
+
+  def randomly_select_power_up():
+    pass
+
+  def power_up_collected():
+    pass
+
+  def shield():
+    pass
+
+  def freeze():
+    pass
